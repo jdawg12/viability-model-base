f30c70b5c73618f25b4c8028da3561af21597838
Joshua Wang <37763916+jdawg12@users.noreply.github.com>
2020-07-22 11:46:41 -0400

    Form.js now working. Can now retrieve data

--- src/components/Form.js
+++ src/components/Form.js
@@ -11,14 +11,14 @@ class Form extends Component {
         }
     }
 
-    // handleUsernameChange = (e) => {
-    //     this.setState({
-    //         username : e.target.value
+    handleUsernameChange = (e) => {
+        this.setState({
+            username : e.target.value
             
-    //     })
-    //     console.log(this.username)
+        })
+        console.log(this.username)
 
-    // }
+    }
 
     handleConfidenceSelect = (e) => {
         this.setState({
@@ -28,24 +28,30 @@ class Form extends Component {
         console.log(this.confidence)
 
     }
+
+    handleSubmit = (e) => {
+        alert(`${this.state.username} ${this.state.confidence}`)
+    }
     
 
     render() {
         return (
-            <form>
-                {/* <div>
+            <form onSubmit={this.handleSubmit}>
+                <div>
                     <label>Username</label>
                     <input type ='text' placeholder='Enter RSN here' value={this.state.username} onChange = {this.handleUsernameChange}/>
-                </div> */}
+                </div>
 
                 <div>
                     <label>Confidence</label>
-                    <select value={this.state.confidence} onChange = {this.handleConfidenceSelect}>
-                        <option value ="asdasd"></option>
-                        <option value ="2"></option>
-                        <option value ="3"></option>
+                    <select value={this.state.confidence}  onChange = {this.handleConfidenceSelect}>
+                        <option value ='' disabled></option>
+                        <option value ="1">1</option>
+                        <option value ="2">2</option>
+                        <option value ="3">3</option>
                     </select>
                 </div>
+                <button type='submit'>Submit</button>
             </form>
             
         )
